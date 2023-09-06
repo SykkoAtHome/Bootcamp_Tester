e6ce434d414b7054e77b5ef2fc4e92fc574757e1
Rafał Tłuchowski <rafal@tluchowski.pl>
2023-09-06 22:04:22 +0200

    prework-07

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String someText = "Sample text";
+        System.out.println(someText);
+        int someNumber1  = 552354;
+        System.out.println(someNumber1);
+        boolean isActive = true;
+        System.out.println(isActive);
+        char idxLetter = 'P';
+        System.out.println(idxLetter);
+        double sampleDouble = 3.14159265359;
+        System.out.println(sampleDouble);
+        int someNumber2  = 1010101;
+        System.out.println(someNumber2);
+        boolean isNew = false;
+        System.out.println(isNew);
+    }
+}
