f13c3a7210e433d3e49491f1b7fb1b36db4609ae
Rafał Tłuchowski <rafal@tluchowski.pl>
2023-09-06 19:55:04 +0200

    HelloWorld-0.7

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("Rafal");
+    System.out.println(43);
+    System.out.println('T');
+
+}
+}
