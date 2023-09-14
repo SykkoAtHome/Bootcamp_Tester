b9ed8c554b40b68b3a30c4c379242bced69cb13d
Rafał Tłuchowski <rafal@tluchowski.pl>
2023-09-14 15:56:54 +0200

    2.4

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,20 +1,34 @@
 public class FirstClass {
     public static void main(String[] args) {
-       Notebook Asus = new Notebook(600, 990, 2020);
-       Notebook Apple = new Notebook(2550, 1760, 2021);
-       Notebook Lenovo = new Notebook(1300, 189, 2023);
+//       Notebook Asus = new Notebook(600, 990, 2020);
+//       Notebook Apple = new Notebook(2550, 1760, 2021);
+//       Notebook Lenovo = new Notebook(1300, 189, 2023);
+//
+//       Asus.checkPrice();
+//       Apple.checkPrice();
+//       Lenovo.checkPrice();
+//
+//       Asus.checkWeight();
+//       Apple.checkWeight();
+//       Lenovo.checkWeight();
+//
+//       Asus.checkValue();
+//       Apple.checkValue();
+//       Lenovo.checkValue();
 
-       Asus.checkPrice();
-       Apple.checkPrice();
-       Lenovo.checkPrice();
+       Grades Polski = new Grades();
+       Grades Matma = new Grades();
+       Polski.addGrade(5);
+       Polski.addGrade(3);
+       Polski.addGrade(4);
+       Matma.addGrade(1);
+       Polski.addGrade(1);
+       Matma.addGrade(3);
+       System.out.println("Polski.lastGrade: " +Polski.lastGrade());
+       System.out.println("Matma.lastGrade: "+Matma.lastGrade());
+       System.out.println("Polski.averageGrade: " +Polski.averageGrade());
+       System.out.println("Matma.averageGrade: " +Matma.averageGrade());
 
-       Asus.checkWeight();
-       Apple.checkWeight();
-       Lenovo.checkWeight();
-
-       Asus.checkValue();
-       Apple.checkValue();
-       Lenovo.checkValue();
 
     }
 }
---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,27 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+    public void addGrade(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+    public int lastGrade() {
+        return this.grades[this.size-1];
+    }
+    public double averageGrade() {
+        int sum = 0;
+        for (int i = 0; i < this.size; i++) {
+            sum = sum + this.grades[i];
+        }
+        return (double) sum / this.size;
+    }
+}
