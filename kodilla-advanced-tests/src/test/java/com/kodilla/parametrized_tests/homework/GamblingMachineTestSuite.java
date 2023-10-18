bed3f45cfbfa1d0c735602fc4fdd8ac2a471354f
Rafał Tłuchowski <rafal@tluchowski.pl>
2023-10-18 16:57:53 +0200

    7.2

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -8,37 +8,5 @@ import java.util.HashSet;
 import java.util.Set;
 public class GamblingMachineTestSuite {
     private final GamblingMachine gm = new GamblingMachine();
-    @Test
-    public void shouldReturnCorrectNumberOfWins() throws InvalidNumbersException {
-        Set<Integer> userNumbers = new HashSet<>();
-        userNumbers.add(1);
-        userNumbers.add(2);
-        userNumbers.add(3);
-        userNumbers.add(4);
-        userNumbers.add(5);
-        userNumbers.add(6);
-        int wins = gm.howManyWins(userNumbers);
-        assertTrue(wins >= 0 && wins <= 6);
-    }
-    @Test
-    public void shouldThrowExceptionForNumbersOutOfScope() {
-        Set<Integer> userNumbers = new HashSet<>();
-        userNumbers.add(0);
-        userNumbers.add(2);
-        userNumbers.add(3);
-        userNumbers.add(4);
-        userNumbers.add(5);
-        userNumbers.add(100);
-        assertThrows(InvalidNumbersException.class, () -> gm.howManyWins(userNumbers));
-    }
-    @Test
-    public void shouldThrowExceptionForIncorrectSize() {
-        Set<Integer> userNumbers = new HashSet<>();
-        userNumbers.add(5);
-        userNumbers.add(10);
-        userNumbers.add(15);
-        userNumbers.add(20);
-        userNumbers.add(25);
-        assertThrows(InvalidNumbersException.class, () -> gm.howManyWins(userNumbers));
-    }
+
 }
